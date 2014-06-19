783b34f51bb35d2a2b49111b433cc56056cadba6
askesian <cwigley@gmail.com>
2014-06-19 17:25:21 -0500

    add read-only tinyMCE when noedit is true

--- public/js/common/ui-wysiwyg-dev.js
+++ public/js/common/ui-wysiwyg-dev.js
@@ -1,21 +1,49 @@
 jQuery(function($) {
-	
+
 	if (!window.tinymce)
 		return;
-	
-	var images = $('.field.type-cloudinaryimage,.field.type-cloudinaryimages').find('img[data-wysiwyg=true]');
-	
-	var options = {
+
+	var plugins = [ 'code', 'link' ],
+		toolbar = 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link';
+
+	if (Keystone.wysiwyg.options.enableImages) {
+		plugins.push('image');
+		toolbar += ' | image';
+	}
+
+	if (Keystone.wysiwyg.options.enableCloudinaryUploads) {
+		plugins.push('uploadimage');
+		toolbar += (Keystone.wysiwyg.options.enableImages) ? ' uploadimage' : ' | uploadimage';
+	}
+
+	if (Keystone.wysiwyg.options.additionalButtons) {
+	    var additionalButtons = Keystone.wysiwyg.options.additionalButtons.split(',');
+	    for (var i=0; i<additionalButtons.length; i++) {
+	        toolbar += (' | ' + additionalButtons[i]);
+	    }
+	}
+
+	toolbar += ' | code';
+
+	//init editable wysiwygs
+	tinymce.init({
 		selector: 'textarea.wysiwyg',
 		menubar: false,
-		plugins: [ 'code', 'link', 'image', 'images',],
-		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link ' + ( images.length ? '| images' : '' ) + ' | code',
+		plugins: plugins,
+		toolbar: toolbar,
 		skin: 'keystone',
-	};
-	
-	if (images.length)
-		options.images_list = images;
-	
-	tinymce.init(options);
-	
+		uploadimage_form_url: '/keystone/api/cloudinary/upload'
+	});
+
+	//init non-editable wysiwygs
+	tinymce.init({
+		selector: 'textarea.wysiwyg-noedit',
+		mode: 'textareas',
+		readonly: true,
+		menubar: false,
+		plugins: plugins,
+		toolbar: false,
+		statusbar: false,
+		skin: 'keystone'
+	});
 });
